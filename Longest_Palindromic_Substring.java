026e56378e909a175ecaf84a13238c14bd05f1d1
Priyan Paul Samuel P <96825787+priyan03@users.noreply.github.com>
2025-07-31 11:25:36 +0530

    Longest_Palindromic_Substring.java

--- Longest_Palindromic_Substring.java
+++ Longest_Palindromic_Substring.java
@@ -1,21 +1,21 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int maxLen = 0;
-        String longest = "";
-        List<String> list = new ArrayList<>();
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i + 1; j <= s.length(); j++){
-                String sub = s.substring(i, j);
-                if(isPalindrome(sub)){
-                    list.add(sub);
-                    if(sub.length() > maxLen){
-                        maxLen = sub.length();
-                        longest = sub;
-                    }
+       int maxLen = 0;
+       String longest = "";
+       List<String>list = new ArrayList<>();
+       for(int i = 0; i < s.length(); i++){
+        for(int j = i + 1; j <= s.length(); j++){
+            String sub = s.substring(i, j);
+            if(isPalindrome(sub)){
+                list.add(sub);
+                if(sub.length() > maxLen){
+                    maxLen = sub.length();
+                    longest = sub;
                 }
             }
         }
-        return longest;
+       }
+       return longest;
     }
     private boolean isPalindrome(String str){
         int left = 0, right = str.length() - 1;
