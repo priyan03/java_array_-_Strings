09986d020bc270f8f65fcd6ce3d231238321db13
Priyan Paul Samuel P <96825787+priyan03@users.noreply.github.com>
2025-07-31 12:37:50 +0530

    Create Zigzag conversion.java

---
+++ Zigzag conversion.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1 || s.length() <= numRows) return s;
+        List<StringBuilder> rows = new ArrayList<>();
+        for(int i = 0 ; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
+            int currRow = 0; 
+            boolean goingDown = false;
+            for(char c : s.toCharArray()){
+                rows.get(currRow).append(c);
+                if(currRow == 0 || currRow == numRows - 1)
+                goingDown =! goingDown;
+                currRow += goingDown ? 1 : -1;
+            }
+            StringBuilder result = new StringBuilder();
+            for(StringBuilder row : rows)
+                result.append(row);
+                return result.toString();
+    }
+}
